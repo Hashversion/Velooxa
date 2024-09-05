41ef3db2cbc4e8d66d719459dcd1d78a60911cce
Hashversion <110537156+Hashversion@users.noreply.github.com>
2024-09-06 01:20:30 +0530

    chore(ui): ui adjustments in home and auctions' page

--- packages/nextjs/app/auctions/page.tsx
+++ packages/nextjs/app/auctions/page.tsx
@@ -3,6 +3,27 @@ import s from "./page.module.scss";
 import Spacer from "@/components/layouts/spacer";
 
 const AuctionsPage = () => {
+  const details = [
+    {
+      imageSrc: "/images/tesla-v1.avif",
+      availability: "available",
+      make: "Tesla Cybertruck",
+      owner: "Vaibhav",
+    },
+
+    {
+      imageSrc: "/images/cybertruck.jpg",
+      availability: "available",
+      make: "Tesla Cybertruck",
+      owner: "Vaibhav",
+    },
+    {
+      imageSrc: "/images/tesla.avif",
+      availability: "available",
+      make: "Tesla Cybertruck",
+      owner: "Vaibhav",
+    },
+  ];
   return (
     <>
       <section>
@@ -11,36 +32,23 @@ const AuctionsPage = () => {
             <h1 className={s.heading}>Live Auctions</h1>
           </div>
           <div className={s.auction}>
-            <div className={s.card}>
-              <div className={s.card__image}>
-                <Image src={"/images/tesla-v1.avif"} width={600} height={600} alt="Car Image" />
-              </div>
-              <div className={s.card__details}>
-                <div className={s.card__available}>Available</div>
-                <div className="flex justify-between">
-                  <div>
-                    <div>Tesla Cybertruck</div>
-                    <div>Vaibhav</div>
+            {details.map(detail => (
+              <div key={crypto.randomUUID()} className={s.card}>
+                <div className={s.card__image}>
+                  <Image src={detail.imageSrc} width={600} height={600} alt="Car Image" />
+                </div>
+                <div className={s.card__details}>
+                  <div className={s.card__available}>{detail.availability}</div>
+                  <div className="flex justify-between">
+                    <div>
+                      <div>{detail.make}</div>
+                      <div>{detail.owner}</div>
+                    </div>
+                    <div className=" self-end">3 ETH</div>
                   </div>
-                  <div className=" self-end">3 ETH</div>
                 </div>
               </div>
-            </div>
-            <div className={s.card}>
-              <div className={s.card__image}>
-                <Image src={"/images/tesla-v1.avif"} width={600} height={600} alt="Car Image" />
-              </div>
-              <div className={s.card__details}>
-                <div className={s.card__available}>Available</div>
-                <div className="flex justify-between">
-                  <div>
-                    <div>Tesla Cybertruck</div>
-                    <div>Vaibhav</div>
-                  </div>
-                  <div className=" self-end">3 ETH</div>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </Spacer>
       </section>
